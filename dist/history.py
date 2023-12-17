38aa3f3a50a27f805ef48ab584bc1abf9bc212ae
NathanJargon <metamorphiaSH@gmail.com>
2023-12-17 10:42:22 +0800

    issue -p

--- dist/history.py
+++ dist/history.py
@@ -4,93 +4,69 @@ import sqlite3
 from PIL import Image, ImageTk
 from customtkinter import *
 from database import WaterBillDatabase
+from register import Register
 
-db = WaterBillDatabase()
+class History:
+    def __init__(self, root):
+        self.root = root
+        self.bill_details_var = StringVar()
+        self.db = WaterBillDatabase()
+        self.r = Register()
+        self.background_frame = CTkFrame(self.root, fg_color="gray11", corner_radius=0)
+        self.background_frame.place(relx=.5, rely=0, relwidth=0.2, relheight=1, anchor='nw')
+        self.title_frame = CTkFrame(self.background_frame, fg_color="gray11")
+        self.title_frame.pack(padx=10, pady=5)
+        self.label_name = CTkLabel(self.title_frame, text="Histories", font=("Oswald", 25))
+        self.label_name.grid(row=0, column=0, padx=0, pady=10)
+        self.canvas = Canvas(self.background_frame, bg="gray11", highlightthickness=0)
+        self.scrollbar = Scrollbar(self.background_frame, orient="vertical", command=self.canvas.yview)
+        self.canvas.configure(yscrollcommand=self.scrollbar.set)
+        self.button_frame = Frame(self.canvas, bg="gray11")
+        self.canvas.create_window((0, 0), window=self.button_frame, anchor='nw')
+        self.canvas.pack(side='left', fill='both', expand=True)
+        self.scrollbar.pack(side='right', fill='y')
+        self.fetch_data()
 
-def save_to_database(customer_name, address, email, consumption, current_reading, previous_reading, meter_consumption, bill_amount_php, messages):
-    db.save_to_database(customer_name, address, email, consumption, current_reading, previous_reading, meter_consumption, bill_amount_php, messages)
+    def save_to_database(self, customer_name, address, email, consumption, current_reading, previous_reading, meter_consumption, bill_amount_php, messages):
+        self.db.save_to_database(customer_name, address, email, consumption, current_reading, previous_reading, meter_consumption, bill_amount_php, messages)
 
-def fetch_data():
-    for widget in button_frame.winfo_children():
-        widget.destroy()
+    def fetch_data(self):
+        for widget in self.button_frame.winfo_children():
+            widget.destroy()
 
-    data = db.fetch_data()
-    
-    for row in data:
-        bill_button = CTkButton(button_frame, text=f"Customer: {row[1]}", command=lambda row=row: show_details(row))
-        bill_button.pack(padx=10, pady=5)
+        data = self.db.fetch_data()
 
-def delete_data(id):
-    db.delete_data(id)
-    fetch_data()
-    
-def fetch_data():
-    for widget in button_frame.winfo_children():
-        widget.destroy()
+        for row in data:
+            bill_button = CTkButton(self.button_frame, text=f"Customer: {row[1]}", command=lambda row=row: self.show_details(row))
+            bill_button.pack(padx=10, pady=5)
 
-    #conn = sqlite3.connect("_internal/db/water_bill_database.db")
-    conn = sqlite3.connect("db/water_bill_database.db")
-    
-    cursor = conn.cursor()
-    cursor.execute('SELECT * FROM water_bills')
-    data = cursor.fetchall()
-    conn.close()
-    
-    for row in data:
-        bill_button = CTkButton(button_frame, text=f"Customer: {row[1]}", command=lambda row=row: show_details(row))
-        bill_button.pack(padx=10, pady=5)
+    def delete_and_clear(self, id):
+        self.db.delete_and_clear(id)
+        self.fetch_data()
+
+    def show_details(self, row):
+        bill_details = f"Customer Name: {row[1]}\n"
+        bill_details += f"Address: {row[2]}\n"
+        bill_details += f"Email: {row[3]}\n"
+        bill_details += f"Consumption: {row[4]} gallons\n\n"
+        bill_details += f"Metering Information:\n"
+        bill_details += f"Current Reading: {row[5]}\n"
+        bill_details += f"Previous Reading: {row[6]}\n"
+        bill_details += f"Meter Consumption: {row[7]} gallons\n\n"
+        bill_details += f"Billing Summary:\n"
+        bill_details += f"Total Bill Amount (in PHP): ₱{row[8]:.2f}\n\n"
+        bill_details += f"Message: {row[9]}"
         
-def delete_and_clear(id):
-    #conn = sqlite3.connect("_internal/db/water_bill_database.db")
-    conn = sqlite3.connect("db/water_bill_database.db")
+        self.bill_details_var.set(bill_details)
+        for widget in self.details_frame.winfo_children():
+            if isinstance(widget, CTkButton):
+                widget.destroy()
+
+        delete_button = CTkButton(self.details_frame, text="Delete", command=lambda id=row[0]: self.delete_and_clear(id))
+        delete_button.pack(padx=10, pady=5)
     
-    cursor = conn.cursor()
-    db.delete_and_clear(id)
-    conn.close()
-    fetch_data()
-    
-############################################# CSV Histories
 
-#conn = sqlite3.connect("_internal/db/water_bill_database.db")
-conn = sqlite3.connect("db/water_bill_database.db")
-
-cursor = conn.cursor()
-
-cursor.execute('SELECT * FROM water_bills')
-data = cursor.fetchall()
-
-conn.close()
-
-background_frame = CTkFrame(root, fg_color="gray11", corner_radius=0)
-background_frame.place(relx=.5, rely=0, relwidth=0.2, relheight=1, anchor='nw')
-
-title_frame = CTkFrame(background_frame, fg_color="gray11")
-title_frame.pack(padx=10, pady=5)
-
-label_name = CTkLabel(title_frame, text="Histories", font=("Oswald", 25))
-label_name.grid(row=0, column=0, padx=0, pady=10)
-
-canvas = Canvas(background_frame, bg="gray11", highlightthickness=0)
-scrollbar = Scrollbar(background_frame, orient="vertical", command=canvas.yview)
-canvas.configure(yscrollcommand=scrollbar.set)
-
-button_frame = Frame(canvas, bg="gray11")
-
-if data:
-    for row in data:
-        button_text = f"Customer: {row[1]}"
-        button = CTkButton(button_frame, text=button_text, command=lambda r=row: show_details(r), font=("Oswald", 15))
-        button.pack(padx=(15,10), pady=5)
-else:
-    messagebox.showinfo("No Data", "No water bill data found in the database.")
-
-canvas.create_window((0, 0), window=button_frame, anchor='nw')
-
-
-button_frame.update_idletasks()
-canvas.config(scrollregion=canvas.bbox('all'))
-
-canvas.pack(side='left', fill='both', expand=True)
-scrollbar.pack(side='right', fill='y')
-
-#############################################
+if __name__ == "__main__":
+    root = tk.Tk()
+    history = History(root)
+    root.mainloop()
